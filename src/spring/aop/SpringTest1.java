e017b601a9b4492fb64311fb4e4350c2c70a12d2
hasanov <DKhasanov@lanit.ru>
2016-05-04 10:23:13 +0400

    xml aop2

---
+++ src/spring/aop/SpringTest1.java
@@ -0,0 +1,24 @@
+package spring.aop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import spring.simpleinjecting.MediaPlayer;
+
+/**
+ * Created by Денис on 01.04.2016.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "aopconfig.xml")
+public class SpringTest1 {
+
+    @Autowired
+    Perfomance perfomance;
+
+    @Test
+    public void test(){
+        perfomance.perform();
+    }
+}
